b40c52c113b7fdc9e78652d6c5216ac29e0b87d0
Jesse Crocker <jesse@gaiagps.com>
2016-01-22 16:29:08 -0700

    Add /status/ endpoint for health check

--- staticMaps/staticMaps/urls.py
+++ staticMaps/staticMaps/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import status as status_view
 
 urlpatterns = patterns('',
-	url(r'^', include('mapRender.urls')),
+    url(r'^status/$', status_view),
+	url(r'^', include('mapRender.urls'))
 )
---
+++ staticMaps/staticMaps/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def status(request):
+    return HttpResponse('')
